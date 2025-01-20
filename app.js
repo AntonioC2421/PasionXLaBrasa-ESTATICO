b739e2c6fbd95cc0b5bd2d900e6428e778855de4
AntonioC2421 <cotrina210101@gmail.com>
2025-01-19 23:23:05 -0300

    PASIONXLABRASA

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ app.set("view engine", "ejs");
 // Configura la carpeta 'public' como estática
 app.use(express.static('public'));
 
-app.get("/Ruta1", (req, res) => {
+app.get("/Home", (req, res) => {
     res.render("index")
 });
 
-app.get("/Ruta2", (req, res) => {
+app.get("/Carta", (req, res) => {
+    res.render("Menus")
+});
+
+app.get("/Promociones", (req, res) => {
+    res.render("Menus")
+});
+
+app.get("/Info", (req, res) => {
+    res.render("Menus")
+});
+
+app.get("/Ubicanos", (req, res) => {
     res.render("Menus")
 });
 
