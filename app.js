e7805a3c8f2c8e99e94c146acf6366d98f776824
AntonioC2421 <cotrina210101@gmail.com>
2025-01-20 17:19:41 -0300

    Navegación completa

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
 
 app.set("view engine", "ejs");
+app.use(expressLayouts); // Usar middleware de layouts
+app.set("layout", "layouts/LayoutMain"); // Definir layout principal
 
 // Configura la carpeta 'public' como estática
 app.use(express.static('public'));
@@ -10,20 +13,20 @@ app.get("/Home", (req, res) => {
     res.render("index")
 });
 
+app.get("/Nosotros", (req, res) => {
+    res.render("Nosotros")
+});
+
 app.get("/Carta", (req, res) => {
-    res.render("Menus")
+    res.render("Cartas")
 });
 
 app.get("/Promociones", (req, res) => {
-    res.render("Menus")
-});
-
-app.get("/Info", (req, res) => {
-    res.render("Menus")
+    res.render("Promociones")
 });
 
 app.get("/Ubicanos", (req, res) => {
-    res.render("Menus")
+    res.render("Ubicacion")
 });
 
 app.listen(3000, (req, res) => {
